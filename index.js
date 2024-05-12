c8349316a2be128dd6a9a33205b7ba05a09285fa
pradip2004 <pradipsadhukhan16@gmail.com>
2024-05-13 00:10:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "practice",
+  password: "pradip2004",
+  port: 5432,
+})
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    // Check if the country code already exists in visited_countries table
+    const existingCountry = await db.query(
+      "SELECT * FROM visited_countries WHERE country_code = $1",
+      [countryCode]
+    );
+
+    if (existingCountry.rows.length === 0) {
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+        countryCode,
+      ]);
+      res.redirect("/");
+    } else {
+      // Country code already exists, handle it as per your application logic
+      res.send("Country already visited!");
+    }
+  } else {
+    res.send("Country not found!");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
